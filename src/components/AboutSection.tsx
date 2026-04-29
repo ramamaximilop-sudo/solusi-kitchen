58581383a6896f28a4ffaae1d0cfe9a97b81812c
rama <ramamaximilop@gmail.com>
2026-04-29 18:27:51 +0700

    Update AboutSection.tsx

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -92,7 +92,7 @@ export const AboutSection = () => {
             <div className="relative aspect-[4/5] rounded-3xl overflow-hidden shadow-2xl bg-ske-bg">
               <div 
                 style={{ backgroundImage: "url('https://yerutlqkfjjcrfqsmupz.supabase.co/storage/v1/object/public/FOTO%20produck/grok-image-e59c4c94-f697-4970-bb83-5e0a8567619d.png" }}
-                className="absolute inset-0 w-full h-full bg-cover bg-center grayscale opacity-80"
+                className="absolute inset-0 w-full h-full bg-cover bg-center  opacity-80"
               ></div>
               <div className="absolute inset-0 bg-ske-blue/40"></div>
               
